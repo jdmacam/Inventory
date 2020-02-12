eead145ebddc03b0e2eab427d508bbe595ef9a83
Joseph Macam <jdmacam@gmail.com>
2020-02-12 11:17:02 -0800

    fixes #1

--- Inventory.cpp
+++ Inventory.cpp
@@ -4,7 +4,9 @@
 
 using std::string;
 using std::ostream;
-  
+using std::cout;
+using std::endl;
+
 Inventory::Inventory(string name, float price, int count)
 {
   m_name = name;
@@ -14,12 +16,18 @@ Inventory::Inventory(string name, float price, int count)
 
 void Inventory::sell()
 {
-  m_in_stock--;
+  if (m_in_stock == 0){
+    cout << "Sorry, that item is out of stock" << endl;
+  }
+  else {
+    m_in_stock--;
+  }
+  return;
 }
 
 ostream& operator<<(ostream& stream, const Inventory& item)
 {
-  stream << item.m_name 
+  stream << item.m_name
          << " $"
          << std::fixed << std::setprecision(2) << item.m_price;
   return stream;
